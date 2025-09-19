68aedb3d7aa566150d63e2c65a8408461cdc21a3
change the name at local repo <changethemail@atlocal.repo>
2025-09-19 19:29:29 +0200

    install-scalar

--- app/main.py
+++ app/main.py
@@ -13,3 +13,15 @@ def get_shipment():
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():
     return get_scalar_api_reference(openapi_url=app.openapi_url, title="Scalar API")
+
+
+text: str = "value"
+
+number: int | float = 32
+
+
+def root(num: int | float) -> float:
+    return pow(num, 0.5)
+
+
+root_25 = root(24)
